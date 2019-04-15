5612c6abee163535d9218d512c84ad9f6e107274
youname <531850058@qq.com>
2019-04-15 11:43:35 +0800

    工程

--- src/main/java/com/qf/SmartwatchApplication.java
+++ src/main/java/com/qf/SmartwatchApplication.java
@@ -1,8 +1,12 @@
 package com.qf;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan(basePackages = "com.qf.config")
+@MapperScan(basePackages = "com.qf.mapper")
 @SpringBootApplication
 public class SmartwatchApplication {
 
